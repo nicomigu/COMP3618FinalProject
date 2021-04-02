70f5712d2902c27a118d0b32bb97ddce3293441a
Ben Heaps <bheaps99@gmail.com>
2021-04-01 19:49:48 -0700

    fourth

--- SamuraiBattleView/MainWindow.xaml.cs
+++ SamuraiBattleView/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ViewModel;
 
 namespace SamuraiBattleView
 {
@@ -23,6 +24,7 @@ namespace SamuraiBattleView
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new SamuraiBattleViewModel();
         }
     }
 }
